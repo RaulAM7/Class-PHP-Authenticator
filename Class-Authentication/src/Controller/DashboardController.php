ad9b3a9bbaff9dc19b11a827cb9c8526874f12d1
RaulAM <raulartilesm@gmail.com>
2024-11-06 14:02:16 +0000

    Update: checking bug in Dashboard access

--- Class-Authentication/src/Controller/DashboardController.php
+++ Class-Authentication/src/Controller/DashboardController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 
 class DashboardController extends AbstractController
@@ -17,5 +18,6 @@ class DashboardController extends AbstractController
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
         ]);
+
     }
 }
